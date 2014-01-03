e72bfce87d09c9a0a7183e7cae86ff28ef529d86
Cory MacVie <cory@corymacvie.com>
2014-01-03 13:52:29 -0600

    updates to ex24

--- ex24.py
+++ ex24.py
@@ -1,5 +1,5 @@
 print "Let's practice everything."
-print 'You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.'
+print 'You\'d need to know \'bout escapes with \\ that do \nnewlines and \t tabs.'
 
 poem = """
 \t The lovely world
@@ -25,13 +25,13 @@ def secret_formula(started):
 	return jelly_beans, jars, crates
 
 
-	start_point = 10000
-	beans, jars, crates = secret_formula(start_point)
+start_point = 10000
+beans, jars, crates = secret_formula(start_point)
 
-	print "With a starting point of: %d" % start_point
-	print "We'd have %d beans, %d jars, and %d crates." % (beans, jars, crates)
+print "With a starting point of: %d" % start_point
+print "We'd have %d beans, %d jars, and %d crates." % (beans, jars, crates)
 
-	start_point = start_point / 10
+start_point = start_point / 10
 
-	print "We can also do that this way:"
-	print "We'd have %d beans, %d jars, and %d crates." % secret_formula(start_point)
+print "We can also do that this way:"
+print "We'd have %d beans, %d jars, and %d crates." % secret_formula(start_point)
