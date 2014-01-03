c853f1d3c07aa96f6f69e73803fb567ac1b16471
Cory MacVie <cory@corymacvie.com>
2014-01-03 10:00:58 -0600

    ex19, 20

--- ex18.py
+++ ex18.py
@@ -1,7 +1,7 @@
 # this one is like your scripts with argv
-def print_two(*args):
-	arg1, arg2 = args
-	print "arg1: %r, arg2: %r" % (arg1, arg2)
+def print_two(*args):  # def - tells python we want to make a function, then give the function a name (says what it does), *argv like argv parameter but for functions (has to be in side ())
+	arg1, arg2 = args   # become attached to this name. This line unpacks the arguments.
+	print "arg1: %r, arg2: %r" % (arg1, arg2) # prints out the arguments. 
 
 # ok, that *args is actually pointless, we can just do this
 def print_two_again(arg1, arg2):
