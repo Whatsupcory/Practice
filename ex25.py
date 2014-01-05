80ec001921034898627488ca16d090f4200e1b9c
Cory MacVie <cory@corymacvie.com>
2014-01-04 23:15:11 -0600

    additional updates

---
+++ ex25.py
@@ -0,0 +1,18 @@
+def break_words(stuff):
+	"""This function will break up words for us."""
+	words - staff.split(' ')
+	return words
+
+def sort_words(words):
+	"""Sorts the words."""
+	return sorted(words)
+
+def print_first_word(words):
+	"""Prints the first word after popping it off."""
+	words - words.pop(0)
+	print word
+
+def print_last_word(words):
+	"""Prints the last word after popping it off."""
+	words - words.pop(-1)
+	`
